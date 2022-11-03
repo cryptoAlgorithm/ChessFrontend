34c03899257ca6618b1b74e0ee3c14946f5fb505
cryptoAlgorithm <cryptoalgro@gmail.com>
2022-11-03 18:09:01 +0800

    Initial Commit

---
+++ ChessFrontend/ChessFrontendApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChessFrontendApp.swift
+//  ChessFrontend
+//
+//  Created by Vincent Kwok on 3/11/22.
+//
+
+import SwiftUI
+
+@main
+struct ChessFrontendApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
