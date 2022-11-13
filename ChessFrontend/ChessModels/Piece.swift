db06a1664e0921b8cb18c0e9c156e32dd1bebc40
cryptoAlgorithm <cryptoalgro@gmail.com>
2022-11-13 09:22:06 +0800

    chore: add extensive DocC comments to Piece, PieceSide and PieceType

--- ChessFrontend/ChessModels/Piece.swift
+++ ChessFrontend/ChessModels/Piece.swift
@@ -7,32 +7,60 @@
 
 import Foundation
 
-struct Piece {
-    let type: PieceType
-    let side: PieceSide?
+/// A struct representing a chess piece
+public struct Piece {
+    /// The type of chess piece this struct represents
+    public let type: PieceType
+    /// The side this chess piece belongs to
+    ///
+    /// Will only be `nil` if ``Piece/type`` is ``PieceType/empty``.
+    public let side: PieceSide?
 
-    init(_ type: PieceType = .empty, side: PieceSide) {
+    /// Create a chess piece of a specific type and side
+    public init(_ type: PieceType = .empty, side: PieceSide) {
         self.type = type
         self.side = side
     }
 
-    init() {
+    /// Create an empty chess piece
+    public init() {
         type = .empty
         side = nil
     }
 }
 
-enum PieceSide: String {
+/// Which color's side this piece belongs to
+public enum PieceSide: String {
+    /// The piece belongs to the white side
     case white = "White"
+    /// The piece belongs to the black side
     case black = "Black"
+
+    /// Invert the current side (i.e. a black side becomes white and vice versa)
+    public mutating func invert() {
+        self = self == .white ? .black : .white
+    }
+
+    /// Create an instance with a random side
+    public init() {
+        self = Bool.random() ? .white : .black
+    }
 }
 
-enum PieceType: String {
+/// The type of chess piece that resides at a position
+public enum PieceType: String {
+    /// No chess piece exists at the location
     case empty
+    /// A pawn exists at the location
     case pawn
+    /// A bishop exists at the location
     case bishop
+    /// A knight exists at the location
     case knight
+    /// A rook exists at the location
     case rook
+    /// A queen exists at the location
     case queen
+    /// A king exists at the location
     case king
 }
